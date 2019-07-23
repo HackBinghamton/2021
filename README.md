19bdb1030978c37d0ca747d62e8f5efd6156a258
Anna Levenberg <alevenb1@binghamton.edu>
2019-07-22 20:51:21 -0500

    2019s initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2020s
+HackBU 2020s hackathon registration website
---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+/***************** Waypoints ******************/
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInDown');
+	}, {
+		offset: '55%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+
+});
+
+/***************** Smooth Scrolling ******************/
+(function($) {
+	"use strict"; // Start of use strict
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($anchor.attr('href')).offset().top - 50)
+		}, 1250, 'easeInOutExpo');
+		event.preventDefault();
+	});
+
+	// Highlight the top nav as scrolling occurs
+	$('body').scrollspy({
+		target: '.navbar-fixed-top',
+		offset: 51
+	})
+})(jQuery); // End of use strict
+
+function isEmail(email) {
+	  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	  return re.test(email);
+}
+
+$('#submit').click(function() {
+	var email = $('#email').val();
+	
+	if (isEmail(email)) {
+		$.ajax({
+			type: "POST",
+			url: "https://maker.ifttt.com/trigger/hackbu_2018s/with/key/4aMEP2luxHbKmAvQQ7WMPIaDLyTox6RZI4ZqXA0cF7",
+			data: {'value1': email},
+			error: function() {
+				$('#email').val('');
+			}
+		});
+	}
+	else
+		window.alert("This is not an email.")
+});
